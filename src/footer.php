ba742648d7c8cefa6f3a92b63cb011b247a831e5
ambashar10-star <ambashar10@gmail.com>
2026-02-13 14:41:42 +0200

    Create footer.php with restaurant information
    Added footer section with features, social links, and contact information.

---
+++ src/footer.php
@@ -0,0 +1,51 @@
+<!-- Footer -->
+    <footer class="footer">
+      <div class="footer-features">
+      <div class="feature">
+        <i class="fa-solid fa-wheelchair"></i>
+        <p>Restaurant is wheelchair accessible.</p>
+      </div>
+
+      <div class="feature">
+        <i class="fa-solid fa-wifi"></i>
+        <p>Free Wi-Fi for guests.</p>
+      </div>
+
+      <div class="feature">
+        <i class="fa-solid fa-gift"></i>
+        <p>Gift cards availbale.</p>
+      </div>
+
+      <div class="feature">
+        <i class="fa-solid fa-child-reaching"></i>
+        <p>Family friendly atmosphere.</p>
+      </div>
+      </div>
+
+      <div class="footer-quote">
+        <p>"Your voice helps us do better."</p>
+      </div>
+
+      <div class="footer-social">
+        <a href="#"><i class="fa-brands fa-facebook-f"></i> facebook.com/elitedining</a>
+        <a href="#"><i class="fa-brands fa-instagram"></i> instagram.com/elitedining</a>
+      </div>
+
+      <hr>
+
+      <div class="footer-contact" id="contact">
+        
+        <p><i class="fa-solid fa-location-dot"></i> Address: Kaivokatu 7, 13100 Hämeenlinna</p>
+        <p><i class="fa-solid fa-phone"></i>  Contact Number: 0455 555 555</p>
+        <p><a href=""><i class="fa-solid fa-envelope"></i>  Email: elitedining@gmail.com</a></p>
+      </div>
+
+
+      </footer>
+        <p>© 2026 Elite Dining | All Rights Reserved</p>
+
+
+
+    </body>
+
+</html>
