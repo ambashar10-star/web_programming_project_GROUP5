0b377c6816989dcb1fb84ff1f433c5c992037e4a
Nevindi <nevindirasanja@gmail.com>
2026-02-14 16:41:14 +0200

    Added index.php file into GitHub

---
+++ src/index.php
@@ -0,0 +1,177 @@
+<html>
+    <?php include 'header.php'; ?>
+    <body>
+<!-- Main section -->
+        <section1 class="main" id="home">
+        <h2>Fine Dining Experience</h2>
+            <img src="Images2/wall.jpg">
+        </section1>
+
+
+        <section2 class="about">
+                <h2>About Us</h2>
+                    <p>
+                        Elite Dining offers a premium culinary experience with carefully crafted dishes,
+                        fresh ingredients, and a luxurious atmosphere.
+                    </p>
+        </section2>
+
+<!-- Menu -->
+    <section class="menu" id="menu1">
+        <div class="container">
+        <h2 class="text-center">Today's Special</h2>
+
+        <div class="menu-items">
+            <div class="row">
+            <div class="item">
+                <img class="img-responsive" src="Images2/image1.jpg" >
+                <h4>Pizza</h4>
+                <p>Cheesy Chicken Pizza</p>
+                <p class="price">€28</p>
+            </div>
+
+            <div class="item">
+                <img class="img-responsive" src="Images2/image2.jpg" > 
+                <h4>Salad</h4>
+                <p>with Fresh Veggies</p>
+                <p class="price">€18</p>
+            </div>
+
+            <div class="item">
+                <img class="img-responsive" src="Images2/image3.jpg" >
+                <h4>Cake</h4>
+                <p>Strawberry Cake</p>
+                <p class="price">€12</p>
+
+            </div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+        <section class="menu" id="menu2">
+            <div class="container">
+        <h2 class="text-center">Main Course</h2>
+
+        <div class="menu-items">
+            <div class="row">
+
+            <div class="item">
+                <img class="img-responsive" src="Images2/image4.jpg" >
+                <h4>Grilled meat</h4>
+                <p>with Potatoes and Salad</p>
+                <p class="price">€18</p>
+            </div>
+
+            <div class="item">
+                <img class="img-responsive" src="Images2/image5.jpg" >
+                <h4>Spaghetti</h4>
+                <p>with Bacon and Cheese</p>
+                <p class="price">€15</p>
+            </div>
+
+            <div class="item">
+                <img class="img-responsive" src="Images2/image6.jpg" >
+                <h4>Battered Shrimp</h4>
+                <p>with Backed Potatoes + Beer</p>
+                <p class="price">€16</p>
+            </div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <section class="menu" id="menu3">
+        <div class="container">
+        <h2 class="text-center">Desserts</h2>
+
+        <div class="menu-items">
+            <div class="row">
+
+            <div class="item">
+                <img class="img-responsive" src="Images2/image7.jpg" >
+                <h4>Cheese Cake</h4>
+                <p>with Blue Berries</p>
+                <p class="price">€14</p>
+            </div>
+
+            <div class="item">
+                <img class="img-responsive" src="Images2/image8.jpg" >
+                <h4>Lava Cake</h4>
+                <p>with Vanilla Ice-Cream</p>
+                <p class="price">€10</p>
+            </div>
+
+            <div class="item">
+                <img class="img-responsive" src="Images2/image9.jpg" >
+                <h4>Mini Pancakes</h4>
+                <p>with Berries</p>
+                <p class="price">€12</p>
+            </div>
+            </div>
+
+        </div>
+        </div>
+    </section>
+
+
+    <section class="menu" id="menu4">
+        <div class="container">
+        <h2 class="text-center">Beverages</h2>
+
+        <div class="menu-items">
+        <div class="row">
+
+
+            <div class="item">
+                <img class="img-responsive" src="Images2/image10.jpg">
+                <h4>Oreo Milkshake</h4>
+                <p class="price">€12</p>
+            </div>
+
+
+            <div class="item">
+                <img class="img-responsive" src="Images2/image11.jpg">
+                <h4>Iced-Lemon Tea</h4>
+                <p class="price">€12</p>
+            </div>
+
+
+            <div class="item">
+                <img class="img-responsive" src="Images2/image12.jpg">
+                <h4>Mango Mocktail</h4>
+                <p class="price">€12</p>
+            </div>
+            </div>
+
+        </div>
+        </div>
+    </section>
+
+
+    <section class="menu" id="menu5">
+        <div class="container">
+        <h2 class="text-center">Add Onn</h2>
+
+        <div class="menu-items">
+            <div class="row">
+
+            <div class="item">
+                <img class="img-responsive" src="Images2/image13.jpg" >
+                <h4>Extra Cheese</h4>
+                <p class="price">€10</p>
+            </div>
+
+            <div class="item">
+                <img class="img-responsive" src="Images2/image14.jpg" >
+                <h4>Extra Whipping Cream</h4>
+                <p class="price">€12</p>
+            </div>
+
+            </div>
+            </div>
+        </div>
+    </section>
+ </body>
+ <?php include 'footer.php'; ?>
+</html>
