92d35b38d0a11ac7fa819e420ee9f1f16f6586b9
Nevindi <nevindirasanja@gmail.com>
2026-02-19 15:32:48 +0200

    added update.php for order_form

--- src/update.php
+++ src/update.php
@@ -1,1 +1,45 @@
+<?php
+include 'db.php'; 
+$sql = "SELECT * FROM Order_list";
+$result = $conn->query($sql);
+?>
+<html>
+<head>
+    <style>
+        a.top { margin-right:20px; font-size:20px; color:red; }
+    </style>
+<title>Update Data</title>
+</head>
+<body>
+    <a href="create.php" class="top">Create Data</a>
+    <a href="update.php" class="top">Update/Delete Data</a>
+    <a href="read.php" class="top">Retrieve Data</a>
+<hr>
+<table border="1" cellpadding="5">
+<tr>
+<th>table_number</th><th>dish</th>
+<th>quantity</th><th>Edit</th>
+<th>Delete</th>
+</tr>
+<?php 
+if($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()){
+?>
+<tr>
+<td><?php echo ($row["table_number"]); ?></td>
+<td><?php echo ($row["dish"]); ?></td>
+<td><?php echo ($row["quantity"]); ?></td>
+<td><a href="updatesingle.php?id=<?php echo $row['order_id']; ?>">Update</a></td>
+<td><a href="delete.php?id=<?php echo $row['order_id']; ?>">Delete</a></td>
+</tr>
+<?php 
+    }
+} else {
+    echo "<tr><td colspan='5'>No results</td></tr>";
+}
+$conn->close();
+?>
+</table>
+</body>
+</html>
 
