da50257ca75aea0bc9e4056763b151d16359d957
Nevindi <nevindirasanja@gmail.com>
2026-02-19 14:13:33 +0200

    Added order form validation for table number and quantity

--- src/style.js
+++ src/style.js
@@ -1,1 +1,15 @@
 
+    function validatingForm(){
+        let table_number = document.forms["form"]["table_number"].value;
+        let quantity = document.forms["form"]["quantity"].value;
+
+        if(table_number == ""){
+            alert("Table Number must be filled out");
+            return false;
+        }
+            if(quantity == ""){
+            alert("Quantity must be filled out");
+            return false;
+        }
+    }
+
