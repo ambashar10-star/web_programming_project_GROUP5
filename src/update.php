c4e54a620628c5871153a10fd3e90203503eda4b
Nevindi <nevindirasanja@gmail.com>
2026-02-19 20:22:31 +0200

    Add classes to update and delete links for styling

--- src/update.php
+++ src/update.php
@@ -8,9 +8,9 @@ $result = $conn->query($sql);
     <style>
         a.top { margin-right:20px; font-size:20px; color:red; }
     </style>
-<title>Update Data</title>
+<title  class="text-center">Update Data</title>
 </head>
-<body>
+<body class="text-center">
     <a href="create.php" class="top">Create Data</a>
     <a href="update.php" class="top">Update/Delete Data</a>
     <a href="read.php" class="top">Retrieve Data</a>
@@ -29,8 +29,8 @@ if($result->num_rows > 0) {
 <td><?php echo ($row["table_number"]); ?></td>
 <td><?php echo ($row["dish"]); ?></td>
 <td><?php echo ($row["quantity"]); ?></td>
-<td><a href="updatesingle.php?id=<?php echo $row['order_id']; ?>">Update</a></td>
-<td><a href="delete.php?id=<?php echo $row['order_id']; ?>">Delete</a></td>
+<td><a href="updatesingle.php?id=<?php echo $row['order_id']; ?>" class="btn btn-warning btn-sm">Update</a></td>
+<td><a href="delete.php?id=<?php echo $row['order_id']; ?>" class="btn btn-danger btn-sm">Delete</a></td>
 </tr>
 <?php 
     }
