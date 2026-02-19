962467c5aed3bbb56e1f829e0a2f42f5393a192d
Nevindi <nevindirasanja@gmail.com>
2026-02-19 03:20:21 +0200

    created read.php

--- src/read.php
+++ src/read.php
@@ -1,1 +1,33 @@
+<?php
+include 'header.php';
+include 'db.php';
+?>
+<h2 class="text-center mb-4">All Orders</h2>
+<?php
+$sql = "SELECT * FROM Order_list";
 
+if ($result->num_rows > 0) {
+    echo "<table class='table table-bordered'>
+            <thead>
+                <tr>
+                    <th>Table Number</th>
+                    <th>Dish</th>
+                    <th>Quantity</th>
+                </tr>
+            </thead>
+            <tbody>";
+
+while ($row = $result->fetch_assoc()) {
+    echo "<tr>
+            <td>".$row['table_number']."</td>
+            <td>".$row['dish']."</td>
+            <td>".$row['quantity']."</td>
+          </tr>";
+    }
+    echo "</tbody></table>"; 
+}
+    else {
+          echo "No orders found";
+        }
+$conn->close();
+?>
